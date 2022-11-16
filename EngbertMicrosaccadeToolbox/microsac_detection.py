e41c54bd3f57b3154c3d1054ba573950bd9f95ce
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2022-11-16 18:18:33 +0100

    translated microsaccade function

--- EngbertMicrosaccadeToolbox/microsac_detection.py
+++ EngbertMicrosaccadeToolbox/microsac_detection.py
@@ -45,7 +45,6 @@ def vecvel(x, sampling):
     """
     N, _ = x.shape
     v = np.zeros((N, 2))
-    print(v.shape)
     v[2:(N - 2), ] = sampling / 6 * (x[4:N, ] + x[3:(N - 1), ]
                                      - x[1:(N - 3), ] - x[0:(N - 4), ])
     v[1, ] = sampling / 2 * (x[2, ] - x[0, ])
@@ -60,15 +59,97 @@ def smoothdata(x):
     v = v.cumsum(axis=0)
     return v
 
-    
-    
-    
+
+def _test_crit(v, VFAC):
+    # Compute threshold
+    med = np.median(v, axis=0)
+    # median square displacement
+    msd = np.sqrt(np.median((v-med)**2, axis=0))
+    # TODO Warning when too small
+
+    radius = VFAC * msd
+    # Apply test criterion: elliptic treshold
+    test = ((v / radius)**2).sum(axis=1)
+    return test, radius
 
 
+def _identify_saccade_candidates(indx, mindur):
+    # Determine saccades
+    N = len(indx)
+    nsac = 0
+    sac = []
+    dur = 1
+    start = 0
+    k = 0
+    # Loop over saccade candidates
+    while k < N - 1:
+        if indx[k + 1] - indx[k] == 1:
+            dur += 1
+        else:
+            # Minimum duration criterion (exception: last saccade)
+            if dur >= mindur:
+                nsac += 1
+                end = k
+                sac_prop = [indx[start], indx[end]]
+                sac.append(sac_prop)
+            start = k + 1
+            dur = 1
+        k = k + 1
+
+    # Check minimum duration for last microsaccade
+    if dur >= mindur:
+        nsac = nsac + 1
+        end = k
+        sac_prop = [indx[start], indx[end]]
+        sac.append(sac_prop)
+    return sac
 
 
-def microsacc():
-    raise Exception("Not Implemented")
+def microsacc(x, vfac=5, mindur=3, sampling=500):
+    sac_list = []
+    # Compute velocity
+    v = vecvel(x, sampling=sampling)
+
+    # Apply test criterion: elliptic treshold
+    test, radius = _test_crit(v, vfac)
+    indx = np.where(test > 1)[0]
+
+    sac = _identify_saccade_candidates(indx, mindur)
+    nsac = len(sac)
+
+    print(nsac)
+    if nsac > 0:
+        # Compute peak velocity, horiztonal and vertical components
+        for i, s in enumerate(sac):
+            print(i)
+            # Onset and offset for saccades
+            start = int(s[0])
+            end = int(s[1])
+
+            # Saccade peak velocity (vpeak)
+            vpeak = np.max(np.sqrt(v[start:end, 0]**2 + v[start:end, 1]**2))
+            # Saccade vector (dx,dy)
+            dx = x[end, 0] - x[start, 0]
+            dy = x[end, 1] - x[start, 1]
+
+            # Saccade amplitude (dX,dY)
+            # minx <- min(x[idx,1])
+            # maxx <- max(x[idx,1])
+            # miny <- min(x[idx,2])
+            # maxy <- max(x[idx,2])
+            # ix1 <- which.min(x[idx,1])
+            # ix2 <- which.max(x[idx,1])
+            # iy1 <- which.min(x[idx,2])
+            # iy2 <- which.max(x[idx,2])
+            # dX <- sign(ix2-ix1)*(maxx-minx)
+            # dY = sign(iy2-iy1)*(maxy-miny)
+            # sac[s,6:7] = c(dX,dY)
+            sac_prop = [start, end, vpeak, dx, dy]
+            sac_list.append(sac_prop)
+            # in theory also the radius
+    return(sac_list)
+
+
 
 
 
--- tests/test_against_r.py
+++ tests/test_against_r.py
@@ -1,6 +1,11 @@
 import numpy as np
 from EngbertMicrosaccadeToolbox import microsac_detection
 
+"""This is a set of legacy tests, to ensure the exact numerical result is the
+same in the R version and in the python version. These tests rely on data files
+obtained by running the R version of the code.
+"""
+
 
 def test_smoothdata():
     input_array = np.load("tests/xr.npy")
@@ -14,3 +19,33 @@ def test_vecvel():
     expected = np.genfromtxt("tests/xrs_vecvel.dat")
     result = microsac_detection.vecvel(input_array, sampling=500)
     assert np.allclose(expected, result)
+
+
+def test_test_crit():
+    input_array = np.load("tests/xr.npy")
+    v = microsac_detection.vecvel(input_array, sampling=500)
+    test, radius = microsac_detection._test_crit(v, VFAC=5)
+    expected = np.genfromtxt("tests/xrs_criterion.dat")
+    assert np.allclose(expected, test)
+
+
+def test_identify_saccade_candidates():
+    input_array = np.load("tests/xr.npy")
+    v = microsac_detection.vecvel(input_array, sampling=500)
+    test, radius = microsac_detection._test_crit(v, VFAC=5)
+    indx = np.where(test > 1)[0]
+    sac = microsac_detection._identify_saccade_candidates(indx, 3)
+    expected = np.genfromtxt("tests/sac_cand.dat")[:, 0:2]
+    # all indexes are off by 1, because python is 0 indexed
+    assert np.allclose(expected - 1, np.array(sac))
+
+
+def test_microsac():
+    input_array = np.load("tests/xr.npy")
+    sac = microsac_detection.microsacc(input_array)
+    expected = np.genfromtxt("tests/xrs_sac.dat")
+    res = np.array(sac)
+    #offbyone
+    res[:, 0:2] = res[:, 0:2] + 1
+    print(res)
+    assert np.allclose(expected[:, 0:5], res)
